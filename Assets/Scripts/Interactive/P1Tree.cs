93f3128dfb518ea06211edc51566a6e0e4445de9
junyangtong <2052524102@qq.com>
2024-05-18 17:45:41 +0800

    第二版打包

--- Assets/Scripts/Interactive/P1Tree.cs
+++ Assets/Scripts/Interactive/P1Tree.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class P1Tree : Interactive
 {
     public GameObject twig;
+    public GameObject twigOld;
     protected override void OnClickedAction()
     {
         //掉落树枝
         twig.SetActive(true);
+        twigOld.SetActive(false);
     }
 }
